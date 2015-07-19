02f1bebee07ac8d1c2369db45435c795a2a631d9
Nathan-Bateman <nathan.bateman.jr@gmail.com>
2015-07-19 15:21:21 -0500

    deleted comments that were here before

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,26 +1,5 @@
-/* feedreader.js
- *
- * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
- */
-
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
- */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
@@ -66,7 +45,7 @@ $(function() {
          * the icon and then checks to see if the .menu-hidden class
          * is added back to the body tag
          */
-          it('menu displays on click and disappears on another click', function (){
+          it('menu toggles visibility on click', function (){
             $('.menu-icon-link').trigger('click');
             expect($('body').hasClass('menu-hidden')).toBe(false);
             $('.menu-icon-link').trigger('click');
