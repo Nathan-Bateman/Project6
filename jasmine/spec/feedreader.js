acae96e79a8080636db1b088246d0b72171d44ce
Nathan-Bateman <nathan.bateman.jr@gmail.com>
2015-07-20 14:17:01 -0500

    added semicolon, refactored final test, deleted variable in global scope

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -9,7 +9,7 @@ $(function() {
          * in the array, then it makes sure the key value is not an empty string or
          * some value other than a string, such as an integer or boolean.
          */
-         it('urls there and not empty', function(){
+         it('have urls there and not empty', function(){
             for (var i = 0; i < allFeeds.length; i++) {
                 expect(allFeeds[i].url).toBeDefined();
                 expect(allFeeds[i].url).not.toBe('');
@@ -17,7 +17,7 @@ $(function() {
             }
          });
 
-         it('names there and not empty', function(){
+         it('have names there and not empty', function(){
             for (var i = 0; i < allFeeds.length; i++) {
                 expect(allFeeds[i].name).toBeDefined();
                 expect(allFeeds[i].name).not.toBe('');
@@ -28,15 +28,11 @@ $(function() {
 
 //Test to see if the menu is hidden by default, not hidden on a click, and then hidden again after another click
     describe('The menu', function() {
-        /*variable that references the class type and returns a 
-         *boolean of true or false
-         */
-         var visible = $('body').hasClass('menu-hidden');
         /*Tests to see if the visible variable returns true
          *for the body tag. As long as it does, the test should pass.
          */
-         it('menu hidden', function(){
-            expect(visible).toBe(true);
+         it('is hidden by default', function(){
+            expect($('body').hasClass('menu-hidden')).toBe(true);
          });
         /* this test references the icon link and triggers a click
          * using jQuery then checks to see that the .menu-hidden
@@ -45,7 +41,7 @@ $(function() {
          * the icon and then checks to see if the .menu-hidden class
          * is added back to the body tag
          */
-          it('menu toggles visibility on click', function (){
+          it('toggles visibility on click', function (){
             $('.menu-icon-link').trigger('click');
             expect($('body').hasClass('menu-hidden')).toBe(false);
             $('.menu-icon-link').trigger('click');
@@ -68,7 +64,7 @@ $(function() {
           * length of the array containing all elements with those
           * classes and makes sure that it is greater than 0
           */
-         it('feeds displayed?', function(){
+         it('have feeds displayed', function(){
             var len = $("div.feed").find("article.entry").length;
             expect(len).toBeGreaterThan(0);
         });     
@@ -77,37 +73,30 @@ $(function() {
 //Test to see if the selecting a new feed will indeed display a new feed on the screen
     describe('New Feed Selection', function() {
          /* this beforeEach function loads the first feed in the 
-          * list and then stores the value of the first h2 element
-          * in a variable, the done function is obviously to make
+          * list, the done function is obviously to make
           * certain that this callback function is finished before
           * moving on to any of the expectations
           */
          beforeEach(function(done){
-            loadFeed(0,function (){
-                contents = $('.feed').find('h2').html();
+            loadFeed(1,function (){
                     done();
             });
         });
          /* this makes sure that after the beforeEach finishes,
           * the a different feed is loaded, in this case the 2nd feed,
           * finally, after the second feed loads the test expects the 
-          * cached string in the before each function to not be equal to 
-          * the the string of the first h2 element on the new page
+          * cached string initiated before the second feed is loaded 
+          * to not be equal to the the string of the 
+          * first h2 element on the new page
           */   
-         it('new feed displays', function(done) {
-            loadFeed(1,function(){
+         it('displays when a new feed is loaded', function(done) {
+            var contents = $('.feed').find('h2').html();
+            loadFeed(0,function(){
             expect($('.feed').find('h2').html()).not.toEqual(contents);
             done();
 
             });
         });
-         /* this function assures that after all is said and done,
-          * the initial feed is loaded back to the screen
-          */
-         afterAll(function(){
-            loadFeed(0);
-         })
-
     });
 }());
 
